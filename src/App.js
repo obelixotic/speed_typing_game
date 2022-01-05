7bb06fa1a07cad143d7ebe7381da353bae21c7c4
Tushar Goyal <obelixotic@gmail.com>
2022-01-04 21:09:13 -0500

    basic DOM elements of the game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css'
 
 function App() {
   return (
     <div>
-      
+      <h1>How fast can you type?</h1>  
+      <textarea />
+      <h4>Time remaining: ???</h4>
+      <button>Start</button>
+      <h1>Word count: ???</h1>
     </div>
   );
 }

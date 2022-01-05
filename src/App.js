2b96bba23bf45a6ac1b269aa986bcef54c85a2a9
Tushar Goyal <obelixotic@gmail.com>
2022-01-04 23:33:57 -0500

    typing in textarea + word counting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import React, { useState } from "react"
 import './styles.css'
 
 function App() {
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(10)
+
+  function handleChange(event) {
+    const { value } = event.target
+    setText(value)
+  }
+
+  function calculateWordCount(text) {
+    const words = text.trim().split(" ")
+    const filteredWords = words.filter(word => word !== "") //to convert an empty array from [''] --> []
+    console.log(filteredWords)
+    return filteredWords.length
+  }
+
   return (
     <div>
       <h1>How fast can you type?</h1>  
-      <textarea />
-      <h4>Time remaining: ???</h4>
-      <button>Start</button>
+      <textarea 
+        value={text}
+        onChange={handleChange}
+      />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={() => calculateWordCount(text)}>Start</button>
       <h1>Word count: ???</h1>
     </div>
   );

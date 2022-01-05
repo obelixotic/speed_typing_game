fc6d981906c526399140fe38bc716799e9b6be58
Tushar Goyal <obelixotic@gmail.com>
2022-01-05 00:26:46 -0500

    basic functionality, button and textarea enable/disable, css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react"
 import './styles.css'
 
 function App() {
+  const STARTING_TIME = 15
   const [text, setText] = useState("")
-  const [timeRemaining, setTimeRemaining] = useState(3)
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
   const [isTimeRunning, setIsTimeRunning] = useState(false)
   const [wordCount, setWordCount] = useState(0)
 
@@ -17,27 +18,44 @@ function App() {
     const filteredWords = words.filter(word => word !== "") //to convert an empty array from [''] --> []
     return filteredWords.length
   }
-  
+
+  function startGame() {
+    setIsTimeRunning(true)
+    setTimeRemaining(STARTING_TIME)
+    setText("")
+    setWordCount(0)
+  }
+
+  function endGame() {
+    setIsTimeRunning(false)
+    setWordCount(calculateWordCount(text))
+  }
+
   useEffect(() => {
-    if(isTimeRunning && timeRemaining > 0){
+    if (isTimeRunning && timeRemaining > 0) {
       setTimeout(() => {
         setTimeRemaining(currentTime => currentTime - 1)
       }, 1000);
     } else if (timeRemaining === 0) {
-      setIsTimeRunning(false)
-      setWordCount(calculateWordCount(text))
+      endGame()
     }
   }, [isTimeRunning, timeRemaining])
 
   return (
     <div>
-      <h1>How fast can you type?</h1>  
-      <textarea 
+      <h1>How fast can you type?</h1>
+      <textarea
         value={text}
         onChange={handleChange}
+        disabled={!isTimeRunning}
       />
       <h4>Time remaining: {timeRemaining}</h4>
-      <button onClick={() => setIsTimeRunning(true)}>Start</button>
+      <button
+        onClick={startGame}
+        disabled={isTimeRunning}
+      >
+        Start
+      </button>
       <h1>Word count: {wordCount}</h1>
     </div>
   );
